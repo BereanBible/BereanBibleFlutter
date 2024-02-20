0157009ad53eaaa90c38b7e424a8b3a5c98cba7d
Cahleeb <78446583+Caleb-Parks@users.noreply.github.com>
2024-02-20 16:48:00 -0500

    Implemented different behavior for desktop with the page changing buttons

--- berean_bible_app/lib/pages/BibleReaderPage.dart
+++ berean_bible_app/lib/pages/BibleReaderPage.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 import 'package:berean_bible_app/widgets/BibleReaderContent.dart';
 import 'package:berean_bible_app/widgets/BibleBottomNavBar.dart';
-
+import 'package:berean_bible_app/main.dart';
 
 class BibleReaderPage extends StatefulWidget {
   @override
@@ -12,27 +14,47 @@ class _BibleReaderPageState extends State<BibleReaderPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      // Column is also a layout widget. It takes a list of children and
-      // arranges them vertically. By default, it sizes itself to fit its
-      // children horizontally, and tries to be as tall as its parent.
-      //
-      // Column has various properties to control how it sizes itself and
-      // how it positions its children. Here we use mainAxisAlignment to
-      // center the children vertically; the main axis here is the vertical
-      // axis because Columns are vertical (the cross axis would be
-      // horizontal).
-      //
-      // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-      // action in the IDE, or press "p" in the console), to see the
-      // wireframe for each widget.
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Expanded(child: BibleReaderContent()),
-        SafeArea(
-          child: BibleBottomNavBar()
-        )
-      ],
-    );
+    
+    return ResponsiveBuilder(builder: (context, sizingInformation) {
+      return Column(
+        // Column is also a layout widget. It takes a list of children and
+        // arranges them vertically. By default, it sizes itself to fit its
+        // children horizontally, and tries to be as tall as its parent.
+        //
+        // Column has various properties to control how it sizes itself and
+        // how it positions its children. Here we use mainAxisAlignment to
+        // center the children vertically; the main axis here is the vertical
+        // axis because Columns are vertical (the cross axis would be
+        // horizontal).
+        //
+        // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+        // action in the IDE, or press "p" in the console), to see the
+        // wireframe for each widget.
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(child: BibleReaderContent()),
+          (sizingInformation.deviceScreenType != DeviceScreenType.desktop) ? 
+            SafeArea(
+              child: ElevatedButton(
+                    onPressed: () {
+                      // Accessing MyHomePage state from MyApp state and calling _changePage function
+                      if (sizingInformation.deviceScreenType != DeviceScreenType.desktop) {
+                        Provider.of<MyAppState>(context, listen: false).changePage(0);
+                      }
+                    },
+                    child: Text('Go to Margin Editor Page'),
+              ),
+            )
+          :
+          SafeArea(
+            child: Text('Nothin')
+          )
+          ,
+          SafeArea(
+            child: BibleBottomNavBar()
+          )
+        ],
+      );
+    });
   }
 }
--- berean_bible_app/lib/pages/MarginEditorPage.dart
+++ berean_bible_app/lib/pages/MarginEditorPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 import 'package:berean_bible_app/widgets/MarginEditorContent.dart';
 import 'package:berean_bible_app/widgets/BibleBottomNavBar.dart';
 import 'package:berean_bible_app/main.dart';
@@ -14,36 +15,46 @@ class _MarginEditorPageState extends State<MarginEditorPage> {
   @override
   Widget build(BuildContext context) {
     
-    return Column(
-      // Column is also a layout widget. It takes a list of children and
-      // arranges them vertically. By default, it sizes itself to fit its
-      // children horizontally, and tries to be as tall as its parent.
-      //
-      // Column has various properties to control how it sizes itself and
-      // how it positions its children. Here we use mainAxisAlignment to
-      // center the children vertically; the main axis here is the vertical
-      // axis because Columns are vertical (the cross axis would be
-      // horizontal).
-      //
-      // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-      // action in the IDE, or press "p" in the console), to see the
-      // wireframe for each widget.
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Expanded(child: MarginEditorContent()),
-        SafeArea(
-          child: ElevatedButton(
-            onPressed: () {
-              // Accessing MyHomePage state from MyApp state and calling _changePage function
-              Provider.of<MyAppState>(context, listen: false).changePage(1);
-            },
-            child: Text('Go to Bible Reader Page'),
-          ),
-        ),
-        SafeArea(
-          child: BibleBottomNavBar()
-        )
-      ],
-    );
+    return ResponsiveBuilder(builder: (context, sizingInformation) {
+      return Column(
+        // Column is also a layout widget. It takes a list of children and
+        // arranges them vertically. By default, it sizes itself to fit its
+        // children horizontally, and tries to be as tall as its parent.
+        //
+        // Column has various properties to control how it sizes itself and
+        // how it positions its children. Here we use mainAxisAlignment to
+        // center the children vertically; the main axis here is the vertical
+        // axis because Columns are vertical (the cross axis would be
+        // horizontal).
+        //
+        // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+        // action in the IDE, or press "p" in the console), to see the
+        // wireframe for each widget.
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(child: MarginEditorContent()),
+          (sizingInformation.deviceScreenType != DeviceScreenType.desktop) ? 
+            SafeArea(
+              child: ElevatedButton(
+                    onPressed: () {
+                      // Accessing MyHomePage state from MyApp state and calling _changePage function
+                      if (sizingInformation.deviceScreenType != DeviceScreenType.desktop) {
+                        Provider.of<MyAppState>(context, listen: false).changePage(1);
+                      }
+                    },
+                    child: Text('Go to Bible Reader Page'),
+              ),
+            )
+          :
+          SafeArea(
+            child: Text('Nothin')
+          )
+          ,
+          SafeArea(
+            child: BibleBottomNavBar()
+          )
+        ],
+      );
+    });
   }
 }
